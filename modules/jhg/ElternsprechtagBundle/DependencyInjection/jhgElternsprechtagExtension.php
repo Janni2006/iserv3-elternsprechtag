477787ee0af8a60e7915500b417389d9f859a03e
Janni2006 <janni.wielpuetz@gmail.com>
2022-03-25 11:11:18 +0100

    search for install issues

--- modules/jhg/ElternsprechtagBundle/DependencyInjection/jhgElternsprechtagExtension.php
+++ modules/jhg/ElternsprechtagBundle/DependencyInjection/jhgElternsprechtagExtension.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace jhg\ElternsprechtagBundle\DependencyInjection;
 
 use IServ\CoreBundle\DependencyInjection\IServBaseExtension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -15,5 +18,19 @@ use IServ\CoreBundle\DependencyInjection\IServBaseExtension;
  */
 class jhgElternsprechtagExtension extends IServBaseExtension
 {
+
+//     /**
+//      * {@inheritDoc}
+//      */
+//     public function load(array $configs, ContainerBuilder $container)
+//     {
+
+// //        $configuration = new Configuration($this->getAlias());
+// //        $config = $this->processConfiguration($configuration, $configs);
+
+//         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+//         $loader->load('services.yml');
+
+//     }
 
 }
